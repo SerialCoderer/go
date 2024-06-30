06525f4e6d80d38f0385176599711e562f945fb0
serialcoderer <anon0234@nomail.com>
2024-06-30 13:16:28 -0400

    adding reverse string lib

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
-module example/user/hello2.0
+module github.com/coolFreight/go/hello2.0
 
 go 1.22
-
-replace example/user/hello2.0 v0.1 => github.com/example/coolFreight/go/hello2.0 v0.1
--- hello.go
+++ hello.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/coolFreight/go/hello2.0/morestrings"
+)
 
 func main() {
-	fmt.Println("Hello, world.")
+	fmt.Println(morestrings.ReverseRunes("Hello, world."))
 }
